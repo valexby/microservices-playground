d17fa15f241c04cee344e9ac88052b9d35f4aa19
valexby <alexvasiuk@gmail.com>
2021-06-06 17:47:50 +0200

    Helm chart foundation for micros

---
+++ TODO.md
@@ -0,0 +1,2 @@
+* Fix job targets in prometheus chat values, after kubernetes services for micro services will be configured
+* Specify promtail scrape config after micro services are up
--- docker-build-services.sh
+++ docker-build-services.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # File the current Host and set it as a ENV variable
 # This host will be supplied to all Docker Containers to resolve external Ports
 export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
